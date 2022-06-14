7c38bc7bb85a676337a687968c5f8664658fd05f
dzterzic1 <dterzic1@etf.unsa.ba>
2022-06-14 22:15:08 +0200

    malo jos sredjen izgled za registraciju

--- Cinestvar/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Cinestvar/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -78,13 +78,15 @@ namespace Cinestvar.Areas.Identity.Pages.Account
             {
                 var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
-                if (PravnoLice)
-                    await _userManager.AddToRoleAsync(user, "Pravno lice");
-                else
-                    await _userManager.AddToRoleAsync(user, "Fizicko lice");
+                
 
                 if (result.Succeeded)
                 {
+                    if (PravnoLice)
+                        await _userManager.AddToRoleAsync(user, "Pravno lice");
+                    else
+                        await _userManager.AddToRoleAsync(user, "Fizicko lice");
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
